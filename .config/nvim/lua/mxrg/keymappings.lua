7ebece09520df4f68fcec45d765d8f067241a3eb
mxrg999 <mxrg999@gmail.com>
2024-09-21 20:14:02 +0200

    adding copilot toggle funcition

--- .config/nvim/lua/mxrg/keymappings.lua
+++ .config/nvim/lua/mxrg/keymappings.lua
@@ -89,3 +89,17 @@ vim.keymap.set(
 -- vim.keymap.set("n", "<leader><leader>", function()
 --     vim.cmd("so")
 -- end)
+
+
+local copilot_on = true
+vim.api.nvim_create_user_command("CopilotToggle", function()
+	if copilot_on then
+		vim.cmd("Copilot disable")
+		print("Copilot OFF")
+	else
+		vim.cmd("Copilot enable")
+		print("Copilot ON")
+	end
+	copilot_on = not copilot_on
+end, { nargs = 0 })
+vim.keymap.set("", "<M-p>", ":CopilotToggle<CR>", { noremap = true, silent = true })
